82e2e32613e09bedf2c49d6c15d3e34c48c4a628
suzybaengsun(孙观源) <suzybaengsun@lexin.com>
2023-06-06 19:26:29 +0800

    feat:测试husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+// module.exports = {
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:react-hooks/recommended',
+//   ],
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+//   plugins: ['react-refresh'],
+//   rules: {
+//     'react-refresh/only-export-components': 'warn',
+//   },
+// }
+// 其他配置参见
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+
+  // add your custom rules here
+  rules: {
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/script-setup-uses-vars': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-v-html': 'off'
+  }
+}
